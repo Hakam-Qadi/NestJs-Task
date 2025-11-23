7332a9e39b06410580e7581e6a52093bb7f38858
Hakam-Qadi <hakam.qadi@ics.jo>
2025-11-23 16:52:51 +0300

    refactor user controller

--- src/components/users/users.controller.ts
+++ src/components/users/users.controller.ts
@@ -5,33 +5,27 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
     @Get('profile')
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
     async getProfile(@Req() req: any) {
         return this.usersService.getProfile(req.user.id);
     }
 
     @Patch('profile')
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
     async updateProfile(@Req() req: any, @Body() updateTaskDto: UpdateUserDto) {
         return this.usersService.update(req.user.id, updateTaskDto);
     }
 
     @Patch('reset-password')
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
     async resetPassword(@Req() req: any, @Body() updateTaskDto: UpdateUserDto) {
         return this.usersService.resetPassword(req.user.id, updateTaskDto);
     }
 
     @Delete('profile')
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
     async deleteAccount(@Req() req: any) {
         return this.usersService.deleteAccount(req.user.id);
     }
